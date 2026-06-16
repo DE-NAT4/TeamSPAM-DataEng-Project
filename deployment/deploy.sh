c411782f0b2aa1d91ad0f370f8abd9731b918367
SarahLSmith <1317046@genstudents.org>
2026-06-16 15:28:18 +0100

    change file structure

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -5,18 +5,28 @@ set -eu
 ### Script to deploy S3 bucket + lambda in cloudformation stack
 ###
 
-#### CONFIGURATION SECTION ####
+### CONFIGURATION SECTION ###
 aws_profile="$1" # e.g. sot-academy, for the aws credentials
 your_name="spam" # Team name
 deployment_bucket="${your_name}-deployment-bucket"
-#### CONFIGURATION SECTION ####
+
+# Find absolute path
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Path to templates
+TEMPLATE_DIR="${SCRIPT_DIR}/templates"
+
+echo ""
+echo "Deploying templates from: ${TEMPLATE_DIR}"
+echo ""
 
 # Create deployment bucket stack
 echo ""
 echo "Doing deployment bucket..."
 echo ""
+
 aws cloudformation deploy --stack-name "${your_name}-deployment-stack" \
-    --template-file deployment-bucket-stack.yml --region eu-west-1 \
+    --template-file "${TEMPLATE_DIR}/deployment-bucket-stack.yml" --region eu-west-1 \
     --capabilities CAPABILITY_IAM --profile ${aws_profile} \
     --parameter-overrides \
       YourName="${your_name}";
@@ -27,8 +37,8 @@ if [ -z "${SKIP_PIP_INSTALL:-}" ]; then
     # Install dependencies from requirements-lambda.txt into src directory with python 3.12
     # On windows may need to use `py` not `python3`
     python3 -m pip install --platform manylinux2014_x86_64 \
-        --target=./src --implementation cp --python-version 3.12 \
-        --only-binary=:all: --upgrade -r requirements-lambda.txt;
+        --target="${SCRIPT_DIR}/src" --implementation cp --python-version 3.12 \
+        --only-binary=:all: --upgrade -r "${SCRIPT_DIR}/requirements-lambda.txt";
 else
     echo ""
     echo "Skipping pip install"
@@ -40,9 +50,9 @@ fi
 echo ""
 echo "Doing packaging..."
 echo ""
-aws cloudformation package --template-file etl-stack.yml \
+aws cloudformation package --template-file "${TEMPLATE_DIR}/etl-stack.yml" \
     --s3-bucket ${deployment_bucket} \
-    --output-template-file etl-stack-packaged.yml \
+    --output-template-file "${TEMPLATE_DIR}/etl-stack-packaged.yml" \
     --profile ${aws_profile};
 
 # Deploy the main ETL stack using the packaged template "etl-stack-packaged.yml"
@@ -50,7 +60,7 @@ echo ""
 echo "Doing etl stack deployment..."
 echo ""
 aws cloudformation deploy --stack-name "${your_name}-etl-stack" \
-    --template-file etl-stack-packaged.yml --region eu-west-1 \
+    --template-file "${TEMPLATE_DIR}/etl-stack-packaged.yml" --region eu-west-1 \
     --capabilities CAPABILITY_IAM \
     --profile ${aws_profile} \
     --parameter-overrides \
